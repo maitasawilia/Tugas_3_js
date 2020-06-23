750cbe7f3968afb37d52900f3d38a7bd30318d73
maitasawilia <maitasaw@gmail.com>
2020-06-23 22:28:16 +0700

    Tugas 3 JS
    Tugas ini kami kumpulkan untuk Niomic

---
+++ tugas3.js
@@ -0,0 +1,12 @@
+function ambil() {
+  var lempar = "diterima";
+  return lempar;
+  function mintadata(a,b){
+    console.log(a);
+    console.log(b);
+  }
+  mintadata("siang","malam");
+  
+}
+var tampung = ambil()
+console.log(tampung);
